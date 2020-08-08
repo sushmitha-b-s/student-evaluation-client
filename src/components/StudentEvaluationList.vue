d84a557eab4fdae5d35d41dd560eb5d82d670bed
Sushmitha BS <bssushmitha26@gmail.com>
2020-08-08 13:39:30 +0200

    refactoring code

--- src/components/StudentEvaluationList.vue
+++ src/components/StudentEvaluationList.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
+    <h2 class="text-h4 my-7">Evaluations</h2>
+
+    <div class="mb-7">
+      <v-btn color="secondary" @click="dialog = true">
+        <v-icon left>mdi-plus</v-icon>
+        <span>Add an evaluation</span>
+      </v-btn>
+
+      <v-dialog v-model="dialog" width="500">
+        <v-card>
+          <v-card-title class="headline">
+            <p>Add an evaluation</p>
+            <v-spacer></v-spacer>
+            <v-btn icon ml-4 @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-card-title>
+
+          <v-card-text>
+            <p>form goes here</p>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </div>
+
     <p v-if="!hasEvaluations">There are no evaluations given for this student.</p>
 
     <div v-else>
@@ -17,7 +42,6 @@
 </template>
 
 <script>
-// import { format } from "date-fns";
 import StudentEvaluationItem from "./StudentEvaluationItem";
 
 export default {
@@ -25,6 +49,12 @@ export default {
 
   components: {
     StudentEvaluationItem
+  },
+
+  data() {
+    return {
+      dialog: false
+    };
   },
 
   props: {
@@ -40,11 +70,5 @@ export default {
       return !!(this.evaluations && this.evaluations.length);
     }
   }
-
-  //   methods: {
-  //     formattedDate(evalDate) {
-  //       return evalDate ? format(new Date(evalDate), "dd MMM yyyy") : "";
-  //     }
-  //   }
 };
 </script>
--- src/views/StudentDetailsContainer.vue
+++ src/views/StudentDetailsContainer.vue
@@ -5,12 +5,12 @@
         <v-icon left>mdi-arrow-left</v-icon>
         <span>back to students list</span>
       </v-btn>
-      <v-card class="grid__container mx-auto" max-width="900px">
-        <figure v-if="student">
+      <v-card class="grid__container mx-auto" max-width="900px" v-if="student">
+        <figure>
           <img :src="student.profilePic" alt="Student Image" />
         </figure>
 
-        <v-card-text class="text-left pa-0 pl-5" v-if="student">
+        <v-card-text class="text-left pa-0 pl-5">
           <v-card-title class="pt-0 text-h4 text--primary">{{ student.name }}</v-card-title>
           <v-card-subtitle>Batch #{{ student.class.batchNo }}</v-card-subtitle>
           <v-card-text class="subtitle-1">
@@ -21,34 +21,7 @@
         </v-card-text>
       </v-card>
 
-      <div>
-        <h2 class="text-h4 my-7">Evaluations</h2>
-
-        <div class="mb-7">
-          <v-btn color="secondary" @click="dialog = true">
-            <v-icon left>mdi-plus</v-icon>
-            <span>Add an evaluation</span>
-          </v-btn>
-
-          <v-dialog v-model="dialog" width="500">
-            <v-card>
-              <v-card-title class="headline">
-                <p>Add an evaluation</p>
-                <v-spacer></v-spacer>
-                <v-btn icon ml-4 @click="dialog = false">
-                  <v-icon>mdi-close</v-icon>
-                </v-btn>
-              </v-card-title>
-
-              <v-card-text>
-                <p>form goes here</p>
-              </v-card-text>
-            </v-card>
-          </v-dialog>
-        </div>
-
-        <StudentEvaluationList :evaluations="student.evaluations" v-if="student" />
-      </div>
+      <StudentEvaluationList :evaluations="student.evaluations" v-if="student" />
     </v-container>
   </div>
 </template>
@@ -65,12 +38,6 @@ export default {
   },
 
   props: ["classId", "studentId"],
-
-  data() {
-    return {
-      dialog: false
-    };
-  },
 
   created() {
     this.$store.dispatch("student/fetch", this.studentId);
